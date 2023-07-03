a8df0ca9a78b4fd8a4c47a48722c4e4239c271f2
Baik <baik.kaishan@gmail.com>
2023-07-03 22:35:54 +0800

    static n elapsed time

--- script.js
+++ script.js
@@ -1,90 +1,53 @@
-const htmlSlider = document.getElementById("html-slider");
-const cssSlider = document.getElementById("css-slider");
-const jsSlider = document.getElementById("js-slider");
-const reactSlider = document.getElementById("react-slider");
-
-const htmlSelector = document.getElementById("htmlSelector");
-const cssSelector = document.getElementById("cssSelector");
-const jsSelector = document.getElementById("jsSelector");
-const reactSelector = document.getElementById("reactSelector");
+let outputHTML = document.getElementById("clock-html");
+let outputCSS = document.getElementById("clock-css");
+let outputJavascript = document.getElementById("clock-javascript");
+let outputReactjs = document.getElementById("clock-reactjs");
 
 
-const htmlValue = document.getElementById("htmlValue");
-const cssValue = document.getElementById("cssValue");
-const jsValue = document.getElementById("jsValue");
-const reactValue = document.getElementById("reactValue");
+function elapsedHTML(difference){
 
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((difference %  (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((difference %  (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference %  (1000 * 60)) / (1000));
 
-const htmlBar = document.getElementById("htmlBar");
-const cssBar = document.getElementById("cssBar");
-const jsBar = document.getElementById("jsBar");
-const reactBar = document.getElementById("reactBar");
-
-
-
-// function slider() {
-//     valPercent = (mySlider.value / mySlider.max) * 100;
-//     mySlider.style.background = "linear-gradient(to right, #3264fe ${valPercent}%. #d5d5d5 ${valPercent}%";
-//     selector.textContent = mySlider.value;
-// }
-
-comments = [
-    "Still figuring it out!",
-    "Quite decent !",
-    "Solid foundation :)"
-];
-
-
-
-
-htmlSlider.oninput = function hSlider(){
-    htmlValue.innerHTML = htmlSlider.value
-    htmlSelector.style.left = this.value + "%";
-    htmlBar.style.width = this.value + "%";
-    if(this.value >= 70) {
-        document.getElementById("html-comment").innerHTML = comments[2];
-    } else if(this.value <= 40) {
-        document.getElementById("html-comment").innerHTML = comments[0];
-    } else {
-        document.getElementById("html-comment").innerHTML = comments[1];
-    }
+    outputHTML.innerHTML += "Total time elapsed is: " + days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds!"
+    outputCSS.innerHTML += "Total time elapsed is: " + days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds!"
 }
 
-cssSlider.oninput = function cSlider(){
-    cssValue.innerHTML = cssSlider.value
-    cssSelector.style.left = this.value + "%";
-    cssBar.style.width = this.value + "%";
-    if(this.value >= 70) {
-        document.getElementById("css-comment").innerHTML = comments[2];
-    } else if(this.value <= 40) {
-        document.getElementById("css-comment").innerHTML = comments[0];
-    } else {
-        document.getElementById("css-comment").innerHTML = comments[1];
-    }
+let start = new Date("February 21, 2023 08:00:00");
+let end = new Date();
+let difference = end - start;
+elapsedHTML(difference);
+
+
+function elapsedJavascript(differenceJavascript){
+
+    let days = Math.floor(differenceJavascript / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((differenceJavascript %  (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((differenceJavascript %  (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((differenceJavascript %  (1000 * 60)) / (1000));
+
+    outputJavascript.innerHTML += "Total time elapsed is: " + days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds!"
 }
 
-jsSlider.oninput = function jSlider(){
-    jsValue.innerHTML = jsSlider.value
-    jsSelector.style.left = this.value + "%";
-    jsBar.style.width = this.value + "%";
-    if(this.value >= 70) {
-        document.getElementById("js-comment").innerHTML = comments[2];
-    } else if(this.value <= 40) {
-        document.getElementById("js-comment").innerHTML = comments[0];
-    } else {
-        document.getElementById("js-comment").innerHTML = comments[1];
-    }
+let startJavaScript = new Date("April 11, 2023 08:00:00");
+let differenceJavascript = end - startJavaScript;
+
+elapsedJavascript(differenceJavascript);
+
+
+function elapsedReactjs(differenceReactjs){
+
+    let days = Math.floor(differenceReactjs / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((differenceReactjs %  (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((differenceReactjs %  (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((differenceReactjs %  (1000 * 60)) / (1000));
+
+    outputReactjs.innerHTML += "Total time elapsed is: " + days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds!"
 }
 
-reactSlider.oninput = function rSlider(){
-    reactValue.innerHTML = reactSlider.value
-    reactSelector.style.left = this.value + "%";
-    reactBar.style.width = this.value + "%";
-    if(this.value >= 70) {
-        document.getElementById("react-comment").innerHTML = comments[2];
-    } else if(this.value <= 40) {
-        document.getElementById("react-comment").innerHTML = comments[0];
-    } else {
-        document.getElementById("react-comment").innerHTML = comments[1];
-    }
-}
+let startReactjs = new Date("May 21, 2023 08:00:00");
+let differenceReactjs = end - startReactjs;
+
+elapsedReactjs(differenceReactjs);
